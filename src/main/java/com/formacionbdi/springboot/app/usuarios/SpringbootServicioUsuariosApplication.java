57eb5e8e4997269d2fa615d2ce2d6528c478eebb
Saul Avila <saul.avila@nyrhcloud.com>
2021-02-24 20:29:22 -0600

    Se agrega anotacion para habilitar el cliente eureka asi como la
    configuracion de datasource automatica

--- src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -1,11 +1,15 @@
 package com.formacionbdi.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.formacionbdi.springboot.app.usuarios.commons.models.entity"})
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class SpringbootServicioUsuariosApplication {
 
 	public static void main(String[] args) {
